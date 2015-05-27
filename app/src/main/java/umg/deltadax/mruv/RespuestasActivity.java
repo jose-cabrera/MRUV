fc4b07c03d94a734003cbee07967ec0d56d7e838
José Cabrera <deltadax18@gmail.com>
2015-05-26 22:25:48 -0600

    Agregado mostrar la respuesta de MRU en el activity de Respuetas

--- app/src/main/java/umg/deltadax/mruv/RespuestasActivity.java
+++ app/src/main/java/umg/deltadax/mruv/RespuestasActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class RespuestasActivity extends Activity {
@@ -19,10 +20,41 @@ public class RespuestasActivity extends Activity {
     public static final int RESPUESTA_MRUV = 1;
     public static final int RESPUESTA_MRU = 2;
 
+    TextView tv_titulo, tv_mensaje;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_respuestas);
+
+        tv_titulo = (TextView) findViewById(R.id.tv_titulo);
+        tv_mensaje = (TextView) findViewById(R.id.tv_mensaje);
+
+        int respuesta = getIntent().getExtras().getInt(KEY_RESPUESTA);
+
+        String mensaje = "";
+
+        Double distancia = 0.0, tiempo = 0.0, velocidad = 0.0, aceleracion = 0.0, velocidad_inicial = 0.0, velocidad_final = 0.0;
+        switch (respuesta) {
+            case RESPUESTA_MRU:
+                tv_titulo.setText(getResources().getText(R.string.tag_mru_titulo));
+                distancia = getIntent().getExtras().getDouble(KEY_DISTANCIA);
+                velocidad = getIntent().getExtras().getDouble(KEY_VELCODIAD);
+                tiempo = getIntent().getExtras().getDouble(KEY_TIEMPO);
+
+                mensaje += "Distancia: " + distancia;
+                mensaje += "\n";
+                mensaje += "Velocidad: " + velocidad;
+                mensaje += "\n";
+                mensaje += "Tiempo: " + tiempo;
+
+                tv_mensaje.setText(mensaje);
+                break;
+
+            case RESPUESTA_MRUV:
+                tv_titulo.setText(getResources().getText(R.string.tag_mruv_titulo));
+                break;
+        }
     }
 
 }
